1f88e5acf73fec4806ae79cbbaf0aa8a80e50413
deepanjali6111 <145878298+deepanjali6111@users.noreply.github.com>
2026-05-25 22:17:07 +0530

    Update chaidocs_rag.py

--- chaidocs_rag.py
+++ chaidocs_rag.py
@@ -8,7 +8,7 @@ os.environ["ANONYMIZED_TELEMETRY"] = "False"
 from typing import List
 from langchain_core.documents import Document
 from langchain_community.document_loaders import WebBaseLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
 from langchain_core.prompts import ChatPromptTemplate
